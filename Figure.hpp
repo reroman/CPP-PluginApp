a596adad0f71f101d30b4da872d3c888265f0a03
Ricardo Román <reroman4@gmail.com>
2025-01-09 12:11:04 -0600

    Refactor of Figure.hpp.
    Raw pointers are replaced with std::unique_ptr, and setData() is
    replaced with userInput().

--- Figure.hpp
+++ Figure.hpp
@@ -1,69 +1,60 @@
 #ifndef FIGURE_HPP
 #define FIGURE_HPP
 
+#include <memory>
 #include <string>
+#include <string_view>
 
-#define CREATE_FIGURE_FUNCTION	"createFigure"
-#define DESTROY_FIGURE_FUNCTION	"destroyFigure"
+namespace plugin {
 
-namespace plugin{
-	/**
-	 * Abstract class that represents a Figure. Every plugin
-	 * in this application should extends this class.
-	 */
-	class Figure
-	{
-		public:
-			/**
-			 * Initializes a Figure with its name.
-			 */
-			Figure( std::string figureName )
-				: name( figureName ){}
-			virtual ~Figure(){}
-
-			/**
-			 * Gets the name of the figure.
-			 */
-			inline std::string getName( void ) const{ return name; }
-
-			// ==== Methods that must be override ====
-			/**
-			 * Set required values to calculate area and perimeter
-			 */
-			virtual void setData( void ) = 0;
-
-			/**
-			 * Calculate the area of the figure.
-			 * @return The value of the area.
-			 */
-			virtual double area( void ) const = 0;
-
-			/**
-			 * Calculate the perimeter of the figure.
-			 * @return The value of the perimeter.
-			 */
-			virtual double perimeter( void ) const = 0;
-
-		private:
-			std::string name;
-	};
-}
+/** @brief Name of the factory function that creates a Figure object. */
+constexpr auto factory_name = "figureFactory";
 
 /**
- * Every plugin created must implement these two
- * C functions: createFigure and destroyFigure. This macro
- * uses default constructors to create/destroy the figures.
+ * @brief Abstract class that represents a plugin Figure.
+ * @details Every plugin in this application should extends this class and
+ * override the functions userInput(), area() and perimeter(). It also requires
+ * to define a C function figureFactory() that returns a unique pointer to a
+ * Figure object.
  */
-#define DEFAULT_REGISTER_FIGURE( x );		\
-extern "C"{ 						 		\
-									 		\
-plugin::Figure* createFigure(){       		\
-	return new x;                    		\
-}                                    		\
-                                     		\
-void destroyFigure( plugin::Figure *p ){   	\
-	delete p;                        		\
-}									 		\
-}
+class Figure {
+ public:
+  /** @brief Initializes a Figure with its name. */
+  Figure(std::string_view figureName) : name(figureName) {}
+
+  /** @brief Destructor. */
+  virtual ~Figure() = default;
+
+  /** @brief  Gets the name of the figure. */
+  std::string const& getName() const noexcept { return name; }
+
+  /**
+   * @brief Ask the user the required values to calculate area and perimeter.
+   */
+  virtual void userInput() = 0;
+
+  /** @brief Calculate and return the area of the figure. */
+  virtual double area() const = 0;
+
+  /** @brief Calculate and return the perimeter of the figure. */
+  virtual double perimeter() const = 0;
+
+ protected:
+  Figure(Figure const& other) = delete;
+  Figure(Figure&& other) noexcept = default;
+  Figure& operator=(Figure const& other) = delete;
+  Figure& operator=(Figure&& other) noexcept = default;
+
+ private:
+  std::string name;
+};
+
+}  // namespace plugin
+
+/** @brief Default implementation of the figureFactory() function. */
+#define DEFAULT_FIGURE_FACTORY(figure)                         \
+  extern "C" std::unique_ptr<plugin::Figure> figureFactory() { \
+    return std::make_unique<figure>();                         \
+  }
 
 #endif
